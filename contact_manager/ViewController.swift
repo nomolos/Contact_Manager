d6c7a0f8aad360fef626e58c22c17e9db1ed427b
Solomon Demmessie <meagh054@fb.com>
2019-08-29 12:44:38 -0700

    Initial Commit

---
+++ contact_manager/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  contact_manager
+//
+//  Created by meagh054 on 8/29/19.
+//  Copyright © 2019 solomon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
